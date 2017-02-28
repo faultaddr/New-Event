e5e5c7ee58641612ba81d9af55caedefe7417517
panyunyi <panyunyi@outlook.com>
2017-03-01 01:48:00 +0800

    加入remoteVIew更新桌面widget 但是设置 TextView的文字内容的时候出现了一些问题没有办法显示，暂时不知道为什么

---
+++ app/src/main/java/com/example/panda/newevent/provider/NewAppWidget.java
@@ -0,0 +1,47 @@
+package com.example.panda.newevent.provider;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.example.panda.newevent.R;
+import com.example.panda.newevent.service.RemoteViewServiceImp;
+
+/**
+ * Created by panyunyi on 2017/2/28.
+ */
+
+public class NewAppWidget extends AppWidgetProvider {
+    public static final String BTNACTION = "com.xinxue.action.TYPE_BTN";
+    public static final String ITEMCLICK = "com.xinxue.action.TYPE_LIST";
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
+//绑定service用来填充listview中的视图
+        Intent intent = new Intent(context, RemoteViewServiceImp.class);
+        remoteViews.setRemoteAdapter(R.id.listviewWidget, intent);
+        //remoteViews.setTextViewText(R.id.widgetTitle,"111");
+
+        Intent intent1 = new Intent(ITEMCLICK);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent1, PendingIntent.FLAG_CANCEL_CURRENT);
+        remoteViews.setPendingIntentTemplate(R.id.listviewWidget, pendingIntent);
+
+        //如果你添加了多个实例的情况下需要下面的处理
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction().equals(ITEMCLICK)) {
+            Toast.makeText(context, intent.getIntExtra("position", 0) + "", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
